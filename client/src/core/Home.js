6778808e9e7ece0191dae92af93f43d8580f9f9a
Abilash29 <abisiva2906@gmail.com>
2023-01-09 12:08:50 +0530

    final commit

---
+++ client/src/core/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Posts from '../post/Posts';
+
+const Home = () => (
+    <>
+        <div className="container">
+            <Posts />
+        </div>
+        <footer className="page-footer font-small" style={{ background: "#3E4551" }}>
+            
+        </footer>
+    </>
+);
+
+export default Home;
